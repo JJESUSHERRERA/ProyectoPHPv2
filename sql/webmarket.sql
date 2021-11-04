4a41e9f94d45c2ba2f2d5e9b881df5a048af4d50
jjesusherrera <chuy_ybag@hotmail.com>
2021-11-04 01:35:39 -0600

    v1.3

--- sql/webmarket.sql
+++ sql/webmarket.sql
@@ -1,137 +1,118 @@
-﻿# HeidiSQL Dump 
-#
-# --------------------------------------------------------
-# Host:                         127.0.0.1
-# Database:                     webmarket
-# Server version:               10.1.19-MariaDB
-# Server OS:                    Win32
-# Target compatibility:         ANSI SQL
-# HeidiSQL version:             4.0
-# Date/time:                    2017-09-27 22:22:15
-# --------------------------------------------------------
+﻿--DELETE DATABASE webmarket;
+--CREATE DATABASE  webmarket ;
+--USE webmarket;
 
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ANSI,NO_BACKSLASH_ESCAPES';*/
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;*/
+CREATE TABLE categoria (
+  idC      int          AUTO_INCREMENT PRIMARY KEY,
+  nombre   varchar(60)  NOT NULL);
 
+INSERT INTO categoria (idC, nombre) VALUES  (1,'Flash');
+INSERT INTO categoria (idC, nombre) VALUES  (2,'Camara');
+INSERT INTO categoria (idC, nombre) VALUES  (3,'Memoria');
+INSERT INTO categoria (idC, nombre) VALUES	(4,'Tripode');
+INSERT INTO categoria (idC, nombre) VALUES	(5,'Filtro');
+INSERT INTO categoria (idC, nombre) VALUES	(6,'Bolso');
+INSERT INTO categoria (idC, nombre) VALUES	(7,'Mochila');
+INSERT INTO categoria (idC, nombre) VALUES	(8,'Cinto');
+INSERT INTO categoria (idC, nombre) VALUES	(9,'Bateria');
+INSERT INTO categoria (idC, nombre) VALUES	(10,'Zoom');
 
-#
-# Database structure for database 'webmarket'
-#
+/*CREATE TABLE productos (
+  idP         int         AUTO_INCREMENT ,
+  marca       varchar(60) NOT NULL,
+  descripcion varchar(60) NOT NULL,
+  origen      varchar(60) NOT NULL,
+  precio      int         NOT NULL,
+  categoria   int         NOT NULL,
+  PRIMARY KEY (idP,categoria),
+  FOREIGN KEY  (categoria),
+  CONSTRAINT FK_categoria FOREIGN KEY (categoria) REFERENCES categoria (idC)
+);*/
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ "webmarket" /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE "webmarket";
-
-
-#
-# Table structure for table 'categoria'
-#
-
-CREATE TABLE /*!32312 IF NOT EXISTS*/ "categoria" (
-  "idC" int(11) NOT NULL AUTO_INCREMENT,
-  "nombre" varchar(60) NOT NULL,
-  PRIMARY KEY ("idC")
-) AUTO_INCREMENT=11;
-
-
-
-#
-# Dumping data for table 'categoria'
-#
-
-LOCK TABLES "categoria" WRITE;
-/*!40000 ALTER TABLE "categoria" DISABLE KEYS;*/
-REPLACE INTO "categoria" ("idC", "nombre") VALUES
-	(1,'Flash');
-REPLACE INTO "categoria" ("idC", "nombre") VALUES
-	(2,'Camara');
-REPLACE INTO "categoria" ("idC", "nombre") VALUES
-	(3,'Memoria');
-REPLACE INTO "categoria" ("idC", "nombre") VALUES
-	(4,'Tripode');
-REPLACE INTO "categoria" ("idC", "nombre") VALUES
-	(5,'Filtro');
-REPLACE INTO "categoria" ("idC", "nombre") VALUES
-	(6,'Bolso');
-REPLACE INTO "categoria" ("idC", "nombre") VALUES
-	(7,'Mochila');
-REPLACE INTO "categoria" ("idC", "nombre") VALUES
-	(8,'Cinto');
-REPLACE INTO "categoria" ("idC", "nombre") VALUES
-	(9,'Bateria');
-REPLACE INTO "categoria" ("idC", "nombre") VALUES
-	(10,'Zoom');
-/*!40000 ALTER TABLE "categoria" ENABLE KEYS;*/
-UNLOCK TABLES;
+CREATE TABLE productos (
+  idP         int         AUTO_INCREMENT ,
+  marca       varchar(60) NOT NULL,
+  descripcion varchar(60) NOT NULL,
+  origen      varchar(60) NOT NULL,
+  precio      int         NOT NULL,
+  categoria   int         NOT NULL,
+  PRIMARY KEY (idP),
+  CONSTRAINT FK_categoria FOREIGN KEY (categoria) REFERENCES categoria (idC)
+);
 
 
-#
-# Table structure for table 'productos'
-#
-
-CREATE TABLE /*!32312 IF NOT EXISTS*/ "productos" (
-  "idP" int(11) NOT NULL AUTO_INCREMENT,
-  "marca" varchar(60) NOT NULL,
-  "descripcion" varchar(60) NOT NULL,
-  "origen" varchar(60) NOT NULL,
-  "precio" int(11) NOT NULL,
-  "categoria" int(11) NOT NULL,
-  PRIMARY KEY ("idP","categoria"),
-  KEY "FOREIGN" ("categoria"),
-  CONSTRAINT "FK_categoria" FOREIGN KEY ("categoria") REFERENCES "categoria" ("idC")
-) AUTO_INCREMENT=22;
-
-
-
-#
-# Dumping data for table 'productos'
-#
-
-LOCK TABLES "productos" WRITE;
-/*!40000 ALTER TABLE "productos" DISABLE KEYS;*/
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(1,'Xanxi','Flash de Camara','china',200,1);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(2,'Wolf','Camara','alemania',500,2);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(3,'Mahjong','Memoria SD','china',20,3);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(4,'GoPro','Tripode para Camara','usa',50,4);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(5,'Parker','Filtro de Camara','usa',50,5);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(6,'Xaota','Camara','china',400,2);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(7,'Xony','Camara','china',400,2);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(8,'Kingston','Memoria SD','usa',40,3);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(9,'Gordon','Flash para Camara','usa',300,1);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(10,'Shock','Filtro de Camara','alemania',50,5);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(11,'Xuminga','bolso','china',100,6);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(12,'Mahjong','Bolso de Carga','china',80,6);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(13,'Machgulg','Tripode alto.','alemania',55,4);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(14,'Cachilu','Flash economico','china',170,1);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(15,'Shumager','Cinturon multiples bolsillos','alemania',80,8);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(16,' Obscure','Filtro de luz.','usa',55,5);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(17,'LowImpact','Cinturon fijo','usa',55,8);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(18,'MaxCharge','Bateria recargable.','usa',150,9);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(19,'Karcher','Bateria doble.','alemania',165,9);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(20,'Kiokos','Mochila mediana','china',70,7);
-REPLACE INTO "productos" ("idP", "marca", "descripcion", "origen", "precio", "categoria") VALUES
+INSERT INTO productos (idP, marca, descripcion, origen, precio, categoria) VALUES
 	(21,'Xixion','Zoom economico','china',25,10);
-/*!40000 ALTER TABLE "productos" ENABLE KEYS;*/
-UNLOCK TABLES;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE;*/
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;*/
+
+
+/*
+CREATE TABLE login(
+	id_login	int				auto_increment PRIMARY KEY,
+    user		varchar(20)		not null,
+    pwd			varchar(20)		not null
+);
+
+INSERT INTO login VALUES(DEFAULT, 'admin', '0000');
+INSERT INTO login VALUES(DEFAULT, 'user','1234');
+
+SELECT id_login FROM login WHERE user = 'admin';
+SELECT pwd FROM login WHERE id_login = '1';
+
+CREATE TABLE pacientes(
+	id_paciente			int			auto_increment PRIMARY KEY,
+    nombre				varchar(50)	NOT NULL,
+    fecha				date		NULL,
+    fecha_nac			date		NULL,
+    edad				int			NULL,
+    genero				char		NOT NULL,
+    ocupacion			varchar(20)	NULL,
+    lateralidad			char		NOT NULL,
+    nacionalidad		varchar(20)	NULL,
+    religion			varchar(20)	NULL,
+    domicilio			varchar(20)	NULL,
+    telefono			varchar(20)	NULL,
+    email				varchar(40)	NULL,
+    tel_emergencia		varchar(20)	NULL,
+    con_emergencia		varchar(50)	NULL
+);
+*/
